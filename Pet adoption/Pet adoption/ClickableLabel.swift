f9e19aed83c6911503ab11c32f44600a14631050
Deepthi <mekaladeepthi83@gmail.com>
2024-03-07 19:16:49 -0600

    Created classes for label and datetime

---
+++ Pet adoption/Pet adoption/ClickableLabel.swift
@@ -0,0 +1,32 @@
+//
+//  ClickableLabel.swift
+//  Pet adoption
+//
+//  Created by MekalaDeepthi on 3/7/24.
+//
+
+import UIKit
+
+class ClickableLabel: UILabel {
+       var onClick: () -> Void = {}
+    
+       public override init(frame: CGRect) {
+           super.init(frame: frame)
+           isUserInteractionEnabled = true
+       }
+       
+       public required init?(coder: NSCoder) {
+           super.init(coder: coder)
+       }
+       
+       public convenience init() {
+           self.init(frame: .zero)
+       }
+       
+       open override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+           onClick()
+       }
+}
+    
+
+
