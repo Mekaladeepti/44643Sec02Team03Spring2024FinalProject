21b32eea3cf0cfe894ab818649ea45be307b4001
SriL_1234 <lakshmichowdary830@gmail.com>
2024-03-08 15:33:40 -0600

    Srilakshmi Arthimalla: Created Back button cocoa touch class

---
+++ Pet adoption/Pet adoption/CustomViews/BackButton.swift
@@ -0,0 +1,33 @@
+//
+//  BackButton.swift
+//  Pet adoption
+//
+//  Created by Srilakshmi on 3/8/24.
+//
+
+import UIKit
+
+class BackButton: UIButton {
+
+    override func awakeFromNib() {
+        self.addTarget(self, action: #selector(goBack), for: .touchUpInside)
+    }
+    
+    @objc func goBack(sender: UIButton) {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+              let delegate = windowScene.delegate as? SceneDelegate,
+              var topController = delegate.window?.rootViewController
+        else { return }
+        
+        while let presentedViewController = topController.presentedViewController {
+            topController = presentedViewController
+        }
+        
+        if let navigationController = topController as? UINavigationController, navigationController.viewControllers.count > 1 {
+            navigationController.popViewController(animated: true)
+        } else {
+            // If not, dismiss this view controller
+            topController.dismiss(animated: true, completion: nil)
+        }
+    }
+}
