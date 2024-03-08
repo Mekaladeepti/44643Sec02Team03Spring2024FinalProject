a1f533ac672ae7c3b88bccc9df2bc0597be5b5e3
Sambhavi2 <sambhavi2525@gmail.com>
2024-03-08 09:03:50 -0600

    Sambhavi Mamillapalli: Created a DesignableView class

---
+++ Pet adoption/Pet adoption/CustomViews/DesignableView.swift
@@ -0,0 +1,43 @@
+//
+//  DesignableView.swift
+//  Pet adoption
+//
+//  Created by Durga Sambhavi Mamillapalli on 3/8/24.
+//
+
+import UIKit
+@IBDesignable
+class DesignableView: UIView {
+    @IBInspectable var shadowColor: UIColor = UIColor.clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+
+    @IBInspectable var shadowRadius: CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+
+    @IBInspectable var shadowOpacity: CGFloat = 0 {
+        didSet {
+            layer.shadowOpacity = Float(shadowOpacity)
+        }
+    }
+
+    @IBInspectable var shadowOffsetY: CGFloat = 0 {
+        didSet {
+            layer.shadowOffset.height = shadowOffsetY
+        }
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
