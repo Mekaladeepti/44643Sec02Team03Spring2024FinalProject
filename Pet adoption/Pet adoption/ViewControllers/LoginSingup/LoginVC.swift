1a4c54abc8d2c248a8161c759faa37401b62bb09
Sambhavi2 <sambhavi2525@gmail.com>
2024-03-27 15:02:42 -0500

    Sambhavi Mamillapalli : created an outlet for login button

--- Pet adoption/Pet adoption/ViewControllers/LoginSingup/LoginVC.swift
+++ Pet adoption/Pet adoption/ViewControllers/LoginSingup/LoginVC.swift
@@ -17,12 +17,30 @@ class LoginViewController: UIViewController {
     @IBAction func onLockButtonPressed(_ sender: UIButton) {
         
         self.password.isSecureTextEntry.toggle()
-       
-        
+    
         let buttonImageName = password.isSecureTextEntry ? "lock" : "lock.open"
             if let buttonImage = UIImage(systemName: buttonImageName) {
                 sender.setImage(buttonImage, for: .normal)
         }
+    
+    }
+    
+    @IBAction func onLoginButtonClick(_ sender: Any) {
+        if(email.text!.isEmpty) {
+            showAlertView(message: "Please enter your email.")
+            return
+        }else if  (self.password.text!.isEmpty) {
+            showAlertView(message: "Please enter your password.")
+            return
+        
+                
+            }
+        func showAlertView(message: String) {
+                let alert = UIAlertController(title: "Warning", message: message, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                present(alert, animated: true, completion: nil)
+            }
+        
     }
     
 }
