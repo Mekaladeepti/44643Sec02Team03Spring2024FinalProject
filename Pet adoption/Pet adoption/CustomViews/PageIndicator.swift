133c8622f9ee0b78077ca0a4964942fafcb5e09f
Deepthi <mekaladeepthi83@gmail.com>
2024-03-29 14:41:13 -0500

    Created a pageIndicator class

---
+++ Pet adoption/Pet adoption/CustomViews/PageIndicator.swift
@@ -0,0 +1,116 @@
+//
+//  PageIndicator.swift
+//  Pet adoption
+//
+//  Created by MekalaDeepthi on 3/29/24.
+//
+
+import UIKit
+
+public protocol PageIndicatorView: AnyObject {
+    /// View of the page indicator
+    var view: UIView { get }
+
+    /// Current page of the page indicator
+    var page: Int { get set }
+
+    /// Total number of pages of thAnyObjecte indicator
+    var numberOfPages: Int { get set}
+}
+
+extension UIPageControl: PageIndicatorView {
+    public var view: UIView {
+        return self
+    }
+
+    public var page: Int {
+        get {
+            return currentPage
+        }
+        set {
+            currentPage = newValue
+        }
+    }
+
+    open override func sizeToFit() {
+        var frame = self.frame
+        frame.size = size(forNumberOfPages: numberOfPages)
+        frame.size.height = 30
+        self.frame = frame
+    }
+
+    public static func withSlideshowColors() -> UIPageControl {
+        let pageControl = UIPageControl()
+
+        if #available(iOS 13.0, *) {
+            pageControl.currentPageIndicatorTintColor = UIColor { traits in
+                traits.userInterfaceStyle == .dark ? .white : .lightGray
+            }
+        } else {
+            pageControl.currentPageIndicatorTintColor = .lightGray
+        }
+        
+        if #available(iOS 13.0, *) {
+            pageControl.pageIndicatorTintColor = UIColor { traits in
+                traits.userInterfaceStyle == .dark ? .systemGray : .black
+            }
+        } else {
+            pageControl.pageIndicatorTintColor = .black
+        }
+
+        return pageControl
+    }
+}
+
+/// Page indicator that shows page in numeric style, eg. "5/21"
+public class LabelPageIndicator: UILabel, PageIndicatorView {
+    public var view: UIView {
+        return self
+    }
+
+    public var numberOfPages: Int = 0 {
+        didSet {
+            updateLabel()
+        }
+    }
+
+    public var page: Int = 0 {
+        didSet {
+            updateLabel()
+        }
+    }
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialize()
+    }
+
+    private func initialize() {
+        self.textAlignment = .center
+    }
+
+    private func updateLabel() {
+        text = "\(page+1)/\(numberOfPages)"
+    }
+
+    public override func sizeToFit() {
+        let maximumString = String(repeating: "8", count: numberOfPages) as NSString
+        self.frame.size = maximumString.size(withAttributes: [.font: font as Any])
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
